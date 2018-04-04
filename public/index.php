5b7d2bed419d0f7f214c1e409d908dbb0fca5d13
Erik Lacson <elacson@gmail.com>
2018-04-04 17:54:16 +0800

    added closing tag

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
+
+pif (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
     $url  = parse_url($_SERVER['REQUEST_URI']);
@@ -40,3 +41,5 @@ return $response;
 
 // Run app
 $app->run();
+
+?>
